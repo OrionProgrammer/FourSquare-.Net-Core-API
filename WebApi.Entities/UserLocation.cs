3e40ddb435113965860f9e60f8a4983bd3a907bc
OrionAsh <48157873+OrionProgrammer@users.noreply.github.com>
2020-11-07 22:07:49 +0200

    Add files via upload

---
+++ WebApi.Entities/UserLocation.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Entities
+{
+    public class UserLocation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int LocationId { get; set; }
+    }
+}
