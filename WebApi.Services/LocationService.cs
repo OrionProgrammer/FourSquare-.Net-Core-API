649c178b0f4b40f7ff27273350bf3adfec8fcb5b
OrionAsh <48157873+OrionProgrammer@users.noreply.github.com>
2020-11-07 22:09:15 +0200

    Update LocationService.cs

--- WebApi.Services/LocationService.cs
+++ WebApi.Services/LocationService.cs
@@ -35,5 +35,45 @@ namespace WebApi.Services
         {
             return _context.Location.Where(l => l.IsCorOrdinates == false);
         }
+
+        /// <summary>
+        /// save location for user
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public void CreateUserLocation(int userId, int locationId)
+        {
+            //check if user exists
+            var user = _context.Users.FirstOrDefault(x => x.Id == userId);
+
+            if (user == null)
+                return;
+
+            var existing = _context.UserLocation.FirstOrDefault(x => x.UserId == userId && x.LocationId == locationId);
+            if (existing == null)
+            {
+                UserLocation uLocation = new UserLocation();
+                uLocation.UserId = userId;
+                uLocation.LocationId = locationId;
+
+                _context.UserLocation.Add(uLocation);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Location> GetLocationsByUser(int userId)
+        {
+            var locations = from l in _context.Location
+                            from u in _context.UserLocation
+                            where l.Id == u.LocationId && u.UserId == userId
+                            select new Location
+                            {
+                                Id = l.Id,
+                                IsCorOrdinates = l.IsCorOrdinates,
+                                Value = l.Value
+                            };
+
+            return locations;
+        }
     }
 }
